bcdc2d76a848ac1defa547a0b01c85d5072cb7c7
josh-code <fernandes.developer@gmail.com>
2019-06-26 11:49:41 +0530

    Enabeling Origin access

--- startup/routes.js
+++ startup/routes.js
@@ -7,35 +7,11 @@ const users = require("../routes/users");
 const auth = require("../routes/auth");
 const returns = require("../routes/returns");
 const error = require("../middleware/error");
+const cors = require("cors");
 
 module.exports = function(app) {
   //----------------------Setting route handlers--------------------------
-  app.use(function(req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader(
-      "Access-Control-Allow-Origin",
-      "https://hidden-beyond-42098.herokuapp.com"
-    );
-
-    // Request methods you wish to allow
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    );
-
-    // Request headers you wish to allow
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-Requested-With,content-type"
-    );
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader("Access-Control-Allow-Credentials", true);
-
-    // Pass to next layer of middleware
-    next();
-  });
+  app.use(cors({ origin: "https://hidden-beyond-42098.herokuapp.com/" }));
   app.use(express.json());
   app.use("/api/genres", genres);
   app.use("/api/customers", customers);
